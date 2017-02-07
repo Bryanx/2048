63e4a8c354795f7ddc4fc61e334b7e921700a8f2
Bryanx <br.deridder@gmail.com>
2017-02-07 19:50:57 +0100

    het project

--- src/be/kdg/thegame_2048/Main.java
+++ src/be/kdg/thegame_2048/Main.java
@@ -1,11 +1,32 @@
 package be.kdg.thegame_2048;
 
+import be.kdg.thegame_2048.views.views.StartView;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 /**
- * @author Bryan de Ridder
- * @version 1.0 07-02-17 19:38
+ * @author Bryan de Ridder, Jarne Van Aerde
+ * @version 1.0 04-02-17 18:38
  */
-public class Main {
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        //KLASSEN AANAMKEN
+        StartView startView = new StartView();
+
+        //SCENE SAMENSTELLEN
+        Scene scene = new Scene(startView);
+        primaryStage.setScene(scene);
+        //add stylesheet:
+        scene.getStylesheets().add("be/kdg/project_2048/css/stylesheet.css");
+        primaryStage.setMinHeight(700);
+        primaryStage.setMinWidth(450);
+        primaryStage.setTitle("2048");
+        primaryStage.show();
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello");
+        Application.launch(args);
     }
 }
