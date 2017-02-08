c0cba75d2fc4b2e055a9d4f6ace98949eae78376
Jarne <jarne.vanaerde@student.kdg.be>
2017-02-08 17:02:16 +0100

    Klasse Score uitgewerkt.

---
+++ src/be/kdg/thegame_2048/models/Score.java
@@ -0,0 +1,24 @@
+package be.kdg.thegame_2048.models;
+
+/**
+ * @author Jarne Van Aerde
+ * @version 1.0 8/02/2017 16:57
+ */
+public class Score {
+    //EIGENSCHAPPEN
+    private int score;
+
+    //CONSTRUCTORS
+    public Score() {
+        this.score = 0;
+    }
+
+    //METHODEN
+    public int getScore() {
+        return score;
+    }
+
+    public void calculateScore(Block block, Block otherBlock) {
+        this.score += block.getValue() + otherBlock.getValue();
+    }
+}
