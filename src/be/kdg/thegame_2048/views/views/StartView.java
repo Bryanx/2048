13a1bdc38c9d7cf32de1f62a650615cabe406917
Bryanx <br.deridder@gmail.com>
2017-02-08 16:57:23 +0100

    StartView finished (sort of)

--- src/be/kdg/thegame_2048/views/views/StartView.java
+++ src/be/kdg/thegame_2048/views/views/StartView.java
@@ -2,22 +2,22 @@ package be.kdg.thegame_2048.views.views;
 
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
-import javafx.scene.control.ToggleButton;
+import javafx.scene.Cursor;
+import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
 
 /**
  * @author Bryan de Ridder, Jarne Van Aerde
  * @version 1.0 5/02/2017 18:49
  */
 public class StartView extends BorderPane {
+    public static final Color BG_COLOR = Color.rgb(236,196,0);
+    public static final int OVERALL_PADDING = 50;
     private ImageView logo;
-    private ImageView iconNewPlayer;
-    private ToggleButton tBtnNewPlayer;
-    private ImageView iconExistingPlayer;
-    private ToggleButton tBtnExistingPlayer;
+    private Button btnNewPlayer;
+    private Button btnExistingPlayer;
 
     public StartView() {
         initialiseNodes();
@@ -25,26 +25,38 @@ public class StartView extends BorderPane {
     }
 
     private void initialiseNodes() {
+        //Creates the logo at the top of the screen:
         this.logo = new ImageView("be/kdg/thegame_2048/views/views/img/logo.png");
-        this.iconNewPlayer = new ImageView("be/kdg/thegame_2048/views/views/img/ufo.png");
-        this.tBtnNewPlayer = new ToggleButton("New Player");
-        this.iconExistingPlayer = new ImageView("be/kdg/thegame_2048/views/views/img/user.png");
-        this.tBtnExistingPlayer = new ToggleButton("Existing Player");
+
+        //Creates the New Player button and adds an icon:
+        this.btnNewPlayer = new Button();
+        btnNewPlayer.setGraphic(new ImageView("be/kdg/thegame_2048/views/views/img/newplayer.png"));
+        btnNewPlayer.setBackground(new Background(new BackgroundFill(BG_COLOR, CornerRadii.EMPTY, Insets.EMPTY)));
+        btnNewPlayer.setCursor(Cursor.HAND);
+
+        //Creates the Existing Player button and adds an icon:
+        this.btnExistingPlayer = new Button();
+        btnExistingPlayer.setGraphic(new ImageView("be/kdg/thegame_2048/views/views/img/existingplayer.png"));
+        btnExistingPlayer.setBackground(new Background(new BackgroundFill(BG_COLOR, CornerRadii.EMPTY, Insets.EMPTY)));
+        btnExistingPlayer.setCursor(Cursor.HAND);
     }
 
     private void layoutNodes() {
+        //topside of the StartView, contains the 2048 logo:
         BorderPane top = new BorderPane();
         top.setCenter(logo);
-        top.setPadding(new Insets(0,0,100,0));
-
-        GridPane middle = new GridPane();
-        middle.add(iconNewPlayer, 0, 0);
-        middle.add(tBtnNewPlayer, 1, 0);
-        middle.add(iconExistingPlayer, 0, 1);
-        middle.add(tBtnExistingPlayer, 1, 1);
-        middle.setAlignment(Pos.TOP_CENTER);
+        top.setPadding(new Insets(0, 0, 100, 0));
         this.setTop(top);
+
+        //Middle of the StartView, 2 buttons are stacked inside a GridPane:
+        GridPane middle = new GridPane();
+        middle.add(btnNewPlayer, 0, 0);
+        middle.add(btnExistingPlayer, 0, 1);
+        middle.setAlignment(Pos.TOP_CENTER);
         this.setCenter(middle);
-        this.setPadding(new Insets(50));
+
+        //Properties for the entire screen:
+        this.setPadding(new Insets(OVERALL_PADDING));
+        this.setBackground(new Background(new BackgroundFill(BG_COLOR, CornerRadii.EMPTY, Insets.EMPTY)));
     }
 }
