dbac16e6ec41f1e3db3f58b000e10b6b6eca5b71
Bryanx <br.deridder@gmail.com>
2017-02-23 14:19:14 +0100

    added aboutview images

--- src/be/kdg/thegame_2048/views/Game/GamePresenter.java
+++ src/be/kdg/thegame_2048/views/Game/GamePresenter.java
@@ -85,11 +85,11 @@ public class GamePresenter {
                         event.consume();
                 }
                 modelPlayerMananger.setCurrentPlayerScore(modelGame.getScore().getScore());
-                if (firstRun || isMovable()) {
+//                if (firstRun || isMovable()) {
                     updateView(event.getCode());
-                } else {
-                    moveAnimation(event.getCode());
-                }
+//                } else {
+//                    moveAnimation(event.getCode());
+//                }
             }
         });
         view.getBtnRestart().setOnAction(new EventHandler<ActionEvent>() {
