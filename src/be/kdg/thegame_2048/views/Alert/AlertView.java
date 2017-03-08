ae53e38b5cc98049c963b883eea25bf909f601e1
Bryanx <br.deridder@gmail.com>
2017-03-08 12:16:01 +0100

    added icons

--- src/be/kdg/thegame_2048/views/Alert/AlertView.java
+++ src/be/kdg/thegame_2048/views/Alert/AlertView.java
@@ -9,7 +9,6 @@ import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
-import javafx.scene.shape.StrokeType;
 import javafx.scene.text.TextAlignment;
 
 
